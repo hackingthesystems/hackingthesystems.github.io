81c133290f5414e37f3ae5c14bef4c144b9055ed
hackingthesystems <91442571+hackingthesystems@users.noreply.github.com>
2021-09-28 23:59:42 -0400

    Update and rename liveupdate_bookmark1.js to liveupdate_namesite.js

--- js/liveupdate_namesite.js
+++ js/liveupdate_namesite.js
@@ -28,9 +28,6 @@ function getCookie(name) {
     }
     return null;
 }
-function eraseCookie(name) {
-    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
 
 function changeFavicon(src) {
     var link = document.createElement('link'),
