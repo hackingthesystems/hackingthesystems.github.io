18a7babac5dad979c825fbd9a27095a7c96ffb65
hackingthesystems <91442571+hackingthesystems@users.noreply.github.com>
2021-09-29 00:00:16 -0400

    Create liveupdate_deletecookie.js

---
+++ js/liveupdate_deletecookie.js
@@ -0,0 +1,8 @@
+function eraseCookie(name) {
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+eraseCookie("siteTitle")
+eraseCookie("siteFavicon")
+eraseCookie("autoTitle")
+eraseCookie("autoFavicon")
